cf11e05459b41d0361060a0f0c1653644cb23ba9
han semaj <sangho.nah@gmail.com>
2014-02-07 16:13:47 +1300

    mongo helper added

---
+++ nanodata/lib/mongo.py
@@ -0,0 +1,55 @@
+#-*- coding: utf-8 -*-
+
+from pymongo import MongoClient
+
+
+class MongoHelper(object):
+    """A thin wrapper on top of pymongo."""
+    def __init__(self, hosts, name):
+        self.hosts = hosts
+        self.name = name
+
+    def __enter__(self):
+        self.client = MongoClient(host=self.hosts)
+        self.db = self.client[self.name]
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        try:
+            if any((exc_type, exc_value, traceback,)):
+                raise
+        finally:
+            self.client.disconnect()
+
+    def drop(self, coll):
+        if hasattr(self.db, coll):
+            getattr(self.db, coll).drop()
+
+    def write(self, coll, doc):
+        self.db[coll].insert(doc)
+
+    def read(self, coll, query=None):
+        return (self.db[coll].find(query)
+                if query is not None else
+                self.db[coll].find())
+
+    def read_one(self, coll, query=None):
+        return (self.db[coll].find_one(query)
+                if query is not None else
+                self.db[coll].find_one())
+
+
+if __name__ == "__main__":
+    dbname, collname = "mydb", "mycoll"
+    with MongoHelper(("localhost",), dbname) as db:
+        db.drop(collname)
+
+        db.write(collname, {"name": "a"})
+        db.write(collname, {"name": "b"})
+
+        assert sorted([d["name"] for d in db.read(collname)]) == ["a", "b"]
+        assert [d["name"] for d in db.read(collname,
+                                           query={"name": "b"})] == ["b"]
+        assert db.read_one(collname, query={"name": "b"})["name"] == "b"
+
+        db.drop(collname)
