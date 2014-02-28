81160527feacf1ab79b7c5a5f99244d8a088bb13
han semaj <sangho.nah@gmail.com>
2014-02-28 18:09:24 +1300

    two additional recipes for payments

--- nanodata/recipe/recipe03.py
+++ nanodata/recipe/recipe03.py
@@ -3,14 +3,14 @@
 from functools import partial
 from logging import getLogger
 
-from nanodata import config, COLUMN_MAPPING, TYPE_INVOICE, TYPE_PAYMENT
+from nanodata import config, COLUMN_MAPPING, TYPE_PAYMENT
 from nanodata.lib import db, queries as q, dataframe as df, fn
 from nanodata.recipe import yesterday
 
-PLOT_INFO = {"title": "Daily Invoices/Payments",
+PLOT_INFO = {"title": "Daily Payments",
              "kind": "line"}
 X_LABEL = "Date"
-Y_LABEL = "# of documents"
+Y_LABEL = "# of payments"
 
 logger = getLogger(__name__)
 
@@ -22,9 +22,8 @@ def cook():
         docs = db_source.read(config.DB_SOURCE["collection"],
                               query=q.docs(config.OFFSET,
                                            end=yesterday(),
-                                           types=(TYPE_INVOICE,
-                                                  TYPE_PAYMENT,)))
-        logger.debug("invoices/payments from {start} to {end}: "
+                                           types=(TYPE_PAYMENT,)))
+        logger.debug("payments from {start} to {end}: "
                      "{count}".format(start=config.OFFSET,
                                       end=yesterday(),
                                       count=docs.count()))
@@ -32,10 +31,8 @@ def cook():
         # prepare recipe and start cooking!
         f = fn.compose(partial(df.build_df, mapping=COLUMN_MAPPING),
                        partial(df.dt_to_d, keys=("start",)),
-                       partial(df.group_by, keys=("start", "type",)),
-                       partial(df.count, unstack=True),
-                       partial(df.rename_columns, columns=((0, "Invoice"),
-                                                           (1, "Payment"),)))
+                       partial(df.group_by, keys=("start",)),
+                       df.count)
         return f(docs)
 
 
