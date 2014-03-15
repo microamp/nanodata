742e7cef8dac2da77ce1e7a6d20ae9a24836949e
han semaj <sangho.nah@gmail.com>
2014-03-15 14:16:37 +1300

    recipes, 08 and 09, added

--- nanodata/lib/plot.py
+++ nanodata/lib/plot.py
@@ -3,6 +3,7 @@
 from StringIO import StringIO
 
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib import pyplot as plt
 import seaborn as sns
 
 sns.set_color_palette("deep", desat=.9)
@@ -14,6 +15,27 @@ def build_plot(df, xlabel="X-Axis", ylabel="Y-Axis", **kwargs):
     plot.set_ylabel(ylabel)
 
     figure = plot.get_figure()
+    figure.autofmt_xdate()
+    figure.tight_layout()
+
+    canvas = FigureCanvas(figure)
+    png_output = StringIO()
+    canvas.print_png(png_output)
+
+    figure.clear()  # avoid chart from overlapping
+
+    return png_output
+
+
+def build_subplots(df, **kwargs):
+    figure, axes = plt.subplots(nrows=kwargs["rows"], ncols=kwargs["cols"])
+    plt.suptitle(kwargs["title"])
+
+    for column, (x, y) in kwargs["coordinates"].iteritems():
+        df[column].plot(ax=axes[x, y], kind=kwargs["kind"])
+        axes[x, y].set_title(column)
+        axes[x, y].set_xlabel(kwargs["xlabel"])
+
     figure.autofmt_xdate()
     figure.tight_layout()
 
---
+++ nanodata/recipe/recipe09.py
@@ -0,0 +1,65 @@
+#-*- coding: utf-8 -*-
+
+"""
+    nanodata.recipe.recipe08
+    ------------------------
+
+    [Recipe #08]
+
+    Monthly Billing Documents (Total Amount)
+"""
+
+from functools import partial
+from logging import getLogger
+
+from nanodata import (config, COLUMN_MAPPING, TYPE_INVOICE, TYPE_PAYMENT,
+                      TYPE_DEBIT, TYPE_CREDIT,)
+from nanodata.lib import db, queries as q, dataframe as df, fn, plot
+from nanodata.recipe import yesterday
+
+PLOT_FUNC = plot.build_subplots
+PLOT_INFO = {"title": "Monthly Billing Documents (Total Amount)",
+             "kind": "line",
+             "xlabel": "Month",
+             "rows": 2,
+             "cols": 2,
+             "coordinates": {"Invoice": (0, 0),
+                             "Payment": (0, 1),
+                             "Debit": (1, 0),
+                             "Credit": (1, 1)}}
+
+logger = getLogger(__name__)
+
+
+def cook():
+    # read from source
+    with db.DatabaseHelper(config.DB_SOURCE["hosts"],
+                           config.DB_SOURCE["name"]) as db_source:
+        docs = db_source.read(config.DB_SOURCE["collection"],
+                              query=q.docs(config.OFFSET,
+                                           end=yesterday(),
+                                           types=(TYPE_INVOICE,
+                                                  TYPE_PAYMENT,
+                                                  TYPE_DEBIT,
+                                                  TYPE_CREDIT,)))
+        logger.debug("Billing documents from {start} to {end}: "
+                     "{count}".format(start=config.OFFSET,
+                                      end=yesterday(),
+                                      count=docs.count()))
+
+        # prepare recipe and start cooking!
+        f = fn.compose(partial(df.build_df, mapping=COLUMN_MAPPING),
+                       partial(df.to_monthly, key="start"),
+                       partial(df.group_by, keys=("start", "type",)),
+                       partial(df.sum, unstack=True),
+                       partial(df.rename_columns,
+                               columns=((TYPE_INVOICE, "Invoice"),
+                                        (TYPE_PAYMENT, "Payment"),
+                                        (TYPE_DEBIT, "Debit"),
+                                        (TYPE_CREDIT, "Credit"))),
+                       df.fill_na)
+        return f(docs)
+
+
+if __name__ == "__main__":
+    cook()
